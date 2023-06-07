416fea1f93e2c9d6396473cbac96639a0260cc13
Erich Querner <erichquerner@posteo.org>
2023-06-07 16:54:36 +0200

    add list view with on click event

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/OverviewActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/OverviewActivity.java
@@ -3,16 +3,30 @@ package org.dieschnittstelle.mobile.android.skeleton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Arrays;
+
 public class OverviewActivity extends AppCompatActivity {
+
+    private ViewGroup listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+        this.listView = findViewById(R.id.listView);
+        Arrays.asList("das", "ist", "ein", "test").forEach(listItem -> {
+            TextView itemView = (TextView) getLayoutInflater().inflate(R.layout.activity_overview_listitem, null);
+            itemView.setText(listItem);
+            this.listView.addView(itemView);
+            itemView.setOnClickListener(view ->{
+                showMessage("Selected: " + ((TextView)view).getText());
+            });
+        });
     }
 
     protected void showMessage(String msg){
