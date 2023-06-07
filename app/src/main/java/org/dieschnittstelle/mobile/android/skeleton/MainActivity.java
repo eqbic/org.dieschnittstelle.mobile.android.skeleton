51c37f8e21db8e5f9751b95945866bc5a6fa4d0c
Erich Querner <erichquerner@posteo.org>
2023-06-07 15:44:46 +0200

    simple toast pop up by clicking on text

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
@@ -3,6 +3,9 @@ package org.dieschnittstelle.mobile.android.skeleton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.dieschnittstelle.mobile.android.skeleton.R;
 
@@ -12,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView welcomeText = findViewById(R.id.welcomeText);
+        welcomeText.setText(R.string.welcome_message_alternative);
+
+        welcomeText.setOnClickListener(view -> {
+            Toast.makeText(this, "welcome again!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
