e0d2f470c6e4ac273840c7c88593198bb4e36454
Erich Querner <erichquerner@posteo.org>
2023-06-07 16:15:32 +0200

    remove textview

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/OverviewActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/OverviewActivity.java
@@ -3,26 +3,16 @@ package org.dieschnittstelle.mobile.android.skeleton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
-import org.dieschnittstelle.mobile.android.skeleton.R;
-
-public class MainActivity extends AppCompatActivity {
+public class OverviewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView welcomeText = findViewById(R.id.welcomeText);
-        welcomeText.setText(R.string.welcome_message_alternative);
-
-        welcomeText.setOnClickListener(view -> {
-            showMessage("hello");
-        });
+        setContentView(R.layout.activity_overview);
     }
 
     protected void showMessage(String msg){
